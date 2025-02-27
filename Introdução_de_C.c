159ee9cec5948effba6766d3ae64e2afbaaf6402
LucassLunas <lucasslunas@hotmail.com>
2025-02-27 14:33:58 -0300

    Add files via upload

---
+++ Introdução_de_C.c
@@ -0,0 +1,37 @@
+//Estrutura de um programa em linguagem C
+/* #diretivas do pré compilador 
+    exemplo: #include <stdio.h>
+função principal = int main (void) {
+
+
+       corpo da função principal
+	   
+	   
+	
+}
+Outras funções criadas pelo programador
+
+Tipos primitivos:
+	int = inteiro
+	float = ponto flutuante (precisão simples)
+	double = ponto flutuante (dupla precisão)
+	char = um caractere, ex: 'e'
+	long = inteiro grande 
+	
+	Modificadores:
+		usigned - somente positivos
+		register - usa o registrador
+		
+	Máscaras de leitura e escrita
+	"d" e "i" = inteiro
+	"f" = float e double
+	"c" = Um caractere
+	"s" = string, apenas para mostrar 
+	
+  Exemplos:
+  	float saldo;
+  	scanf ("f", & saldo);
+  	printf ("Quantidad: %d\n", 51)
+  	*/
+  	
+// usar .c para salvar arquivo em vez de cpp, se não o programa não roda.
